942812cf82e62d8e1866c69bc2ae15cbff8810e6
greydelta <55234616+greydelta@users.noreply.github.com>
2021-01-29 20:00:00 +0800

    input validation / utility methods

--- src/main/my/utar/edu/ConsoleUI.java
+++ src/main/my/utar/edu/ConsoleUI.java
@@ -215,4 +215,63 @@ public class ConsoleUI {
 		return loginValid;
 	}
 	
+    
+    public int intInputValidation(int lower, int upper) throws IllegalArgumentException {
+        setScanner(new Scanner(System.in));
+        int userInput;
+        
+        if (scan.hasNextLine()){
+        	String tempString = scan.nextLine();
+        	
+        	// Check if blank / only whitespace
+        	if(tempString.isEmpty() == true)
+        		throw new IllegalArgumentException("\nNot allowed to enter blank values!");
+        	else
+        	
+        	// Check if is valid integer
+        	try {
+        		// Convert to integer
+        		userInput = Integer.parseInt(tempString);
+        	}
+        	catch (NumberFormatException e) {
+        		throw new IllegalArgumentException("\nEnter integers Only!");
+        	}
+        	
+        	// Check if range is same, then can only enter that value
+            if(lower == upper) 
+            	if(userInput != lower)
+            		throw new IllegalArgumentException("\nEnter "+lower+" only!");
+            	else
+            		return userInput;
+           
+            // Check if it is within range
+            if (userInput < lower || userInput > upper)
+                throw new IllegalArgumentException("\nEnter values between "+lower+" and "+upper+" only!");
+            else
+                return userInput;
+        } 
+        else
+            throw new IllegalArgumentException("\nEnter integers Only!");
+    }
+    
+    public String stringInputValidation() throws IllegalArgumentException{
+    	setScanner(new Scanner(System.in));
+    	String userInput;
+    	if (scan.hasNextLine()){
+    		userInput = scan.nextLine();
+    		if(userInput.isEmpty() == true || userInput.equals(" ")==true)
+        		throw new IllegalArgumentException("Not allowed to enter blank values!");
+    		else
+    			return userInput;
+    	}
+    	else
+    		throw new IllegalArgumentException("Scanner is closed");	
+    }
+    
+    public void bufferFor5Miliseconds() {
+    	try {
+			TimeUnit.MILLISECONDS.sleep(500);}
+		catch (InterruptedException e1) {
+			System.err.print(e1.getMessage());}
+    }
 }
