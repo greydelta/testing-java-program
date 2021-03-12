ec15fe8be1a0be03ce84e2234557ae347f093401
greydelta <55234616+greydelta@users.noreply.github.com>
2021-03-13 00:30:00 +0800

    added Payment class

---
+++ src/main/my/utar/edu/Payment.java
@@ -0,0 +1,18 @@
+package my.utar.edu;
+
+public class Payment {
+	
+	private String paymentMethod;
+	private String status;
+	private double totalPrice;
+	// Incomplete module
+	
+	public Payment()
+	{
+		
+	}
+	
+	 public String makingPayment(String paymentMethod, String status, double totalPrice) {
+		 return status;
+	 }
+}
